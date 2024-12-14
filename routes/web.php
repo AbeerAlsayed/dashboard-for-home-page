2b7e0da6bee855c25414417b1654f05aec9e1915
3aber96 <abeerosami1996@gmail.com>
2024-12-15 01:15:18 +0300

    integrate admin page

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('front.index');
+Route::name('front.')->group( function () {
+    Route::view('/','front.index')->name('index');
+    Route::view('/about','front.about')->name('about');
+    Route::view('/service','front.service')->name('service');
+    Route::view('/contact','front.contact')->name('contact');
+});
+
+Route::name('admin.')->prefix('admin')->group( function () {
+    Route::view('/','admin.index')->name('index');
+//    Route::view('/about','admin.about')->name('about');
+//    Route::view('/service','admin.service')->name('service');
+//    Route::view('/contact','admin.contact')->name('contact');
 });
 
 Route::get('/dashboard', function () {
